403cb223977960f81ca54850470463d191262176
Anzhelika <angelika0094@gmail.com>
2018-11-06 13:58:37 +0200

    Initial commit

---
+++ components/TestApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class TestApp extends React.Component {
+    render() {
+        return(
+            <h1>Hello world</h1>
+        );
+    }
+}
+
+export default TestApp
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TestApp from "./components/TestApp";
+
+ReactDOM.render(
+    <TestApp />,
+    document.getElementById("root")
+);
